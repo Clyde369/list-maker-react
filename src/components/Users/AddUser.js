4db0a16fcd2a3b5ada2479f71c551546ed558a6d
Clyde1063 <clyde.apples@gmail.com>
2022-08-05 17:35:03 +0200

    styled the adduser page

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import './AddUser.css'
 
 const AddUser = (props) => {
    const addUserHandler = (event) => {
       event.preventDefault();
    }
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor='username'>Username</label>
-      <input type="text" id="username" name="username"/>
-      <label htmlFor='age'>Age (years)</label>
-      <input type="number" id="age" name="age"/>
-      <button type="submit">Add User</button>
-      
-    </form>
+  <form onSubmit={addUserHandler}>
+    <div class="mb-3 formDiv-container">
+      <label for="username" class="form-label">Enter Username</label>
+      <input type="username" class="form-control" id="username" aria-describedby="userNameHelp"/>
+      <div id="userNameHelp" class="form-text">Please Enter your username here</div>
+    </div>
+    <div class="mb-3 formDiv-container">
+      <label for="age" class="form-label">Enter Age</label>
+      <input type="number" class="form-control" id="age" aria-describedby="ageHelp"/>
+      <div id="ageHelp" class="form-text">Please Enter your age here</div>
+    </div>
+  </form>
   )
 }
 
